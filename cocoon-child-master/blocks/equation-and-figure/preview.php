3358c90015885ba96c20eb5331d83ee5641dfb9c
Munehara Masami <programinmin@gmail.com>
2021-01-17 21:22:44 +0900

    add equation and figure block

---
+++ cocoon-child-master/blocks/equation-and-figure/preview.php
@@ -0,0 +1,12 @@
+<div class="block-equation-figure-wrap">
+  <div class="block-equation-figure-container">
+  <span class="equation-figure-container-title"><?php block_field( 'equation-figure-container-title' ); ?></span>
+    <div class="equation-figure-box equation-figure-box-left">
+      <img class="equation-figure-figure" src= "<?php block_field( 'equation-figure-figure' ); ?>" />
+      <span class="equation-figure-caption"><?php block_field( 'equation-figure-caption' ); ?></span>
+    </div>
+    <div class="equation-figure-box equation-figure-box-right">
+      <span class="equation-figure-text"><?php block_field( 'equation-figure-text' ); ?></span>
+    </div>
+  </div>
+</div>
