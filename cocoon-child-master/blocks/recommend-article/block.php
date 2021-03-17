067db267818ff2d459e7c299398d26869424bb09
Munehara Masami <programinmin@gmail.com>
2021-03-17 20:44:10 +0900

    contact-form width and recommend-article block

---
+++ cocoon-child-master/blocks/recommend-article/block.php
@@ -0,0 +1,4 @@
+<div class="recommend-article-box">
+    <span class="recommend-article-check">Check!</span>
+    <p  class="recommend-article-name"><a href=<?php block_field( 'recommend-article-link' ); ?>><?php block_field( 'recommend-article-name' ); ?></a></p>
+</div>
